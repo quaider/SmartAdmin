7aaeda70e55914aec6cbf12f195b5983887cad67
Quaider Zhang <pto.kratos@hotmail.com>
2016-12-19 22:01:48 +0800

    add simple .net mvc validation

--- SmartAdmin/Models/User.cs
+++ SmartAdmin/Models/User.cs
@@ -11,5 +11,23 @@ namespace SmartAdmin.Models
         [RegularExpression(@"^[a-z]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$", ErrorMessage = "This value should be a valid email.")]
         //[DataType(DataType.EmailAddress, ErrorMessage = "This value should be a valid email.")]
         public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "This value is required.")]
+        public string Password { get; set; }
+
+        [MaxLength(10, ErrorMessage = "Less than 10 words.")]
+        public string MaxLength { get; set; }
+
+        [MinLength(5, ErrorMessage = "More than 5 words.")]
+        public string MinLength { get; set; }
+
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "This value length is invalid. It should be between {2} and {1} characters long.")]
+        public string RangeLength { get; set; }
+
+        [Required(ErrorMessage = "This value is required.")]
+        public string Remark { get; set; }
+
+        [Required(ErrorMessage = "You must select at least 1 items.")]
+        public int? MinCheck { get; set; }
     }
 }
