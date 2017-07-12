6b04096b2afebe2ce7987751acef19aedc570ade
Quaider Zhang <pto.kratos@hotmail.com>
2017-07-12 10:19:31 +0800

    Update storageAndIndexDB.js

--- storageAndIndexDB.js
+++ storageAndIndexDB.js
@@ -453,3 +453,48 @@
 
 	return LocalDB;
 }))
+
+/*others*/
+function _database() {
+	this.name = 'myDB';
+	this.version = 1;
+	this.stores = {
+		news: 'id,guid,title,author,cover,time'
+	};
+};
+
+_database.prototype = {
+	initDB: function(migration) {
+		var db = new Dexie(this.name);
+		db.version(this.version).stores(this.stores).upgrade(function(trans) {
+			if(migration && typeof migration === 'function') {
+				console.log('indexDb 执行了迁移逻辑');
+				migration(trans);
+			}
+		});
+
+		return db;
+	},
+
+	/*
+	 * 传入store name序列
+	 */
+	getDB: function() {
+		var stores = {},
+			self = this;
+		//传入 stroe names，如 store1,store2
+		for(var i = 0; i < arguments.length; i++) {
+			var storeName = arguments[i];
+			if(!self.stores[storeName]) continue;
+
+			stores[storeName] = self.stores[storeName];
+		}
+
+		var db = new Dexie(this.name);
+		db.version(this.version).stores(stores);
+
+		return db;
+	}
+};
+
+window.database = new _database();
